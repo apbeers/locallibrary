dfefaab21a01af9c37addb5bca05f76428815cfc
Andrew Beers <apbeers@gmail.com>
2019-03-03 16:16:00 -0600

    part 6

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from catalog.models import BookInstance, Book, Author, Genre
+from django.views import generic
 
-# Create your views here.
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_avaliable = BookInstance.objects.filter(status__exact='a').count()
+
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_avaliable,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'my_book_list'
+    queryset = Book.objects.all()
+    template_name = 'book_list.html'
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+    paginate_by = 1

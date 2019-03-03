e2356f7bde60fbf9bfda2df2dc2530ae28533049
Andrew Beers <apbeers@gmail.com>
2019-03-03 13:56:01 -0600

    part 4

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Author, Genre, Book, BookInstance
+
+admin.site.register(Genre)
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+
+
+admin.site.register(Book, BookAdmin)
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        })
+    )
+
+
+admin.site.register(BookInstance, BookInstanceAdmin)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+admin.site.register(Author, AuthorAdmin)
--- catalog/models.py
+++ catalog/models.py
@@ -14,8 +14,8 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.CharField(max_length=1000, help_text='Enter a brief description of the book')
-    isbn = models.CharField('ISBN', max_length=13, help_text='13 character<a href="https://www.isbn-international.org'
-                                                             '/content/what-isbn"> ISBN number')
+    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international'
+                                                             '.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
 
     def __str__(self):
@@ -23,6 +23,11 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
 
 class BookInstance(models.Model):
@@ -34,7 +39,7 @@ class BookInstance(models.Model):
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
-        ('o', "On Loan"),
+        ('o', 'On loan'),
         ('a', 'Available'),
         ('r', 'Reserved'),
     )
@@ -44,7 +49,7 @@ class BookInstance(models.Model):
         choices=LOAN_STATUS,
         blank=True,
         default='m',
-        help_text="Book availability",
+        help_text='Book availability',
     )
 
     class Meta:
